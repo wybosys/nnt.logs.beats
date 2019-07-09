c6c771124f36f10422422f15dd9fd208a3e92b98
wybosys <wybosys@gmail.com>
2019-07-09 15:58:20 +0800

    fix

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ def Start(level):
     Manager -> 不同层次error的log -> 最低1分钟为粒度:如果日志导出时间<1分钟, 则wait1分钟-可用时间, >1分钟, 则直接开始消费下一轮. 使用配置文件来获得redis的路径和错误码对应的数据库索引
     '''
     # 打开日志redis
-    logs_redis = redis.Redis().from_url(config.LOGS_REDIS + '/' + level)
+    logs_redis = redis.Redis().from_url(config.LOGS_REDIS + '/' + str(level))
     # 打开mqredis
     mq_redis = redis.Redis().from_url(config.MQ_REDIS + '/0')
     # 工作循环
